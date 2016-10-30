de2e951587a2c368540e52719b98c93cd309dfc4
James <jamestron@gmail.com>
2016-10-30 17:39:59 +1300

    added tiltable script, only controllable with keys currently

---
+++ Assets/Scripts/tiltTable.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tiltTable : MonoBehaviour 
+{
+
+	public float turnSpeed = 1.0f;
+
+	void Start () 
+	{
+	
+	}
+
+	void FixedUpdate () 
+	{
+		if (Input.GetKey (KeyCode.LeftArrow))
+			turn (Vector3.forward, 1.0f);
+
+		if (Input.GetKey (KeyCode.RightArrow))
+			turn (Vector3.forward, -1.0f);
+
+		if (Input.GetKey (KeyCode.UpArrow))
+			turn (Vector3.right, 1.0f);
+
+		if (Input.GetKey (KeyCode.DownArrow))
+			turn (Vector3.right, -1.0f);
+		
+	
+	}
+
+	void turn(Vector3 axis, float dir)
+	{
+		transform.RotateAround (transform.position, axis, turnSpeed * dir);
+	}
+
+}
